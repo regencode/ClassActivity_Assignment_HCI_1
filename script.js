88974f607006359ae0aebdb7793e9998351cd351
Thomas Gozalie <114067350+regencode@users.noreply.github.com>
2022-12-16 17:16:02 +0700

    Uploaded with answers

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Please put your answer here
+
+var signupButton = $(".signup-show");
+
+var modal = $('#signup-modal');
+var closeButton = $('#close-modal');
+
+closeButton.click(function() {
+    modal.fadeOut(100);
+});
+
+signupButton.click(function() {
+    modal.fadeIn(100);
+});
